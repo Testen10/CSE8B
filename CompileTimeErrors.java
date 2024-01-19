c22890f3d2f9ce9183cef40b91b5e0f52de6ddd6
Testen10 <minseochu10@gmail.com>
2024-01-18 21:25:32 -0800

    upload PA2

--- CompileTimeErrors.java
+++ CompileTimeErrors.java
@@ -1,3 +1,5 @@
+import tester.*;
+
 class CompileTimeErrors{
     public static String checkNumber(double myNumber){
         if (myNumber > 0){
@@ -9,19 +11,9 @@ class CompileTimeErrors{
         return "The number is zero.";
     }
 
-    public static void main(String[] args){
-
-        // test1
-        String answer1 = checkNumber(1);
-        System.out.println(answer1);
-        
-        // test2
-        String answer2 = checkNumber(0); 
-        System.out.println(answer2);
-
-        // test3
-        String answer3 = checkNumber(-10.5); 
-        System.out.println(answer3);
-    }
+    String answer1 = checkNumber(1);
+    String answer2 = checkNumber(0); 
+    String answer3 = checkNumber(-10.5); 
+    
     
 }
--- ExampleVideos.java
+++ ExampleVideos.java
@@ -1,3 +1,5 @@
+import tester.*;
+
 class Video{
     /*
     A Video should contain
@@ -93,68 +95,87 @@ class User{
 }
 
 class ExampleVideos{
-    public static void main(String[] args){
-        /* In order to test your classes and methods, find four videos from youtube.com with the following properties:
-            Two of them are from the same user
-            The other two are from two different users
-         */
+    
+    /* In order to test your classes and methods, find four videos from youtube.com with the following properties:
+        Two of them are from the same user
+        The other two are from two different users
+        */
 
-        // Users
-        User user1 = new User("KenshiYonezu","Kenshi Yonezu 米津玄師", 7020000, 137);
-        User user2 = new User("ghostandpals","GHOST",430000, 58);
-        User user3 = new User("GuckkastenOfficial","Guckkasten Official", 38400, 64);
+    // Users
+    User user1 = new User("KenshiYonezu","Kenshi Yonezu 米津玄師", 7020000, 137);
+    User user2 = new User("ghostandpals","GHOST",430000, 58);
+    User user3 = new User("GuckkastenOfficial","Guckkasten Official", 38400, 64);
 
-        // Two video from user1
-        Video video1 = new Video("米津玄師 - Lemon Kenshi Yonezu", user1, 3100000, "SX_ViT4Ra7k"); // link: https://www.youtube.com/watch?v=SX_ViT4Ra7k , it can be represented by Video class
-        Video video2 = new Video("米津玄師 - 感電 Kenshi Yonezu - KANDEN", user1, 3100000, "UFQEttrn6CQ"); // link: https://www.youtube.com/watch?v=UFQEttrn6CQ , it can be represented by Video class
+    // Two video from user1
+    Video video1 = new Video("米津玄師 - Lemon Kenshi Yonezu", user1, 3100000, "SX_ViT4Ra7k"); // link: https://www.youtube.com/watch?v=SX_ViT4Ra7k , it can be represented by Video class
+    Video video2 = new Video("米津玄師 - 感電 Kenshi Yonezu - KANDEN", user1, 3100000, "UFQEttrn6CQ"); // link: https://www.youtube.com/watch?v=UFQEttrn6CQ , it can be represented by Video class
 
-        // video from user2
-        Video video3 = new Video("Gumi Eng. & Yohioloid / The Chattering Lack of Common Sense [Original Song]", user2, 190000, "ksW7SuH6IAs"); // link: https://www.youtube.com/watch?v=ksW7SuH6IAs , it can be represented by Video class
+    // video from user2
+    Video video3 = new Video("Gumi Eng. & Yohioloid / The Chattering Lack of Common Sense [Original Song]", user2, 190000, "ksW7SuH6IAs"); // link: https://www.youtube.com/watch?v=ksW7SuH6IAs , it can be represented by Video class
 
-        // video from user3
-        Video video4 = new Video("Guckkasten 국카스텐 [사냥] Music Video", user3, 8200, "kuvQxTq3ccI"); // link: https://www.youtube.com/watch?v=kuvQxTq3ccI , it can be represented by Video class
+    // video from user3
+    Video video4 = new Video("Guckkasten 국카스텐 [사냥] Music Video", user3, 8200, "kuvQxTq3ccI"); // link: https://www.youtube.com/watch?v=kuvQxTq3ccI , it can be represented by Video class
 
-        String yellow = "\u001B[33m" ;
-	    String exit = "\u001B[0m" ;
+    // 1 testing longerThan method in Video class
+    boolean longerThan_test1 = video2.longerThan(video1);
+    boolean longerThan_test2 = video1.longerThan(video3);
 
-        // 1 testing longerThan method in Video class
-        System.out.println(yellow+"Testing longerThan method"+exit);
-        System.out.println("-".repeat(10));
-        System.out.println(video2.longerThan(video1));
-        System.out.println(video1.longerThan(video3));
-        System.out.println("-".repeat(10));
-        System.out.println("");
+    // 2 testing moreLikes method in Video class
+    boolean moreLikes_test1 = video2.moreLikes(video3);
+    boolean moreLikes_test2 = video4.moreLikes(video2);
 
-        // 2 testing moreLikes method in Video class
-        System.out.println(yellow+"Testing moreLikes method"+exit);
-        System.out.println("-".repeat(10));
-        System.out.println(video2.moreLikes(video3));
-        System.out.println(video4.moreLikes(video2));
-        System.out.println("-".repeat(10));
-        System.out.println("");
+    // 3 testing toText method in Video class
+    String toTextVideo_test1 = video3.toText();
+    String toTextVideo_test2 = video4.toText();
 
-        // 3 testing toText method in Video class
-        System.out.println(yellow+"Testing toText method"+exit);
-        System.out.println("-".repeat(10));
-        System.out.println(video3.toText());
-        System.out.println(video4.toText());
-        System.out.println("-".repeat(10));
-        System.out.println("");
+    // 4 testing toLink method in Video class
+    String toLink_test1 = video1.toLink();
+    String toLink_test2 = video2.toLink();
 
-        // 4 testing toLink method in Video class
-        System.out.println(yellow+"Testing toLink method"+exit);
-        System.out.println("-".repeat(10));
-        System.out.println(video1.toLink());
-        System.out.println(video4.toLink());
-        System.out.println("-".repeat(10));
-        System.out.println("");
+    // 5 testing toText method in User class
+    String toTextUser_test1 = user1.toText();
+    String toTextUser_test2 = user2.toText();
+    String toTextUser_test3 = user3.toText();
 
-        // 5 testing toText method in User class
-        System.out.println(yellow+"Testing toText method (User)"+exit);
-        System.out.println("-".repeat(10));
-        System.out.println(user1.toText());
-        System.out.println(user2.toText());
-        System.out.println(user3.toText());
-        System.out.println("-".repeat(10));
-        }
+
+    // // 1 testing longerThan method in Video class
+    // System.out.println(yellow+"Testing longerThan method"+exit);
+    // System.out.println("-".repeat(10));
+    // System.out.println(video2.longerThan(video1));
+    // System.out.println(video1.longerThan(video3));
+    // System.out.println("-".repeat(10));
+    // System.out.println("");
+
+    // // 2 testing moreLikes method in Video class
+    // System.out.println(yellow+"Testing moreLikes method"+exit);
+    // System.out.println("-".repeat(10));
+    // System.out.println(video2.moreLikes(video3));
+    // System.out.println(video4.moreLikes(video2));
+    // System.out.println("-".repeat(10));
+    // System.out.println("");
+
+    // // 3 testing toText method in Video class
+    // System.out.println(yellow+"Testing toText method"+exit);
+    // System.out.println("-".repeat(10));
+    // System.out.println(video3.toText());
+    // System.out.println(video4.toText());
+    // System.out.println("-".repeat(10));
+    // System.out.println("");
+
+    // // 4 testing toLink method in Video class
+    // System.out.println(yellow+"Testing toLink method"+exit);
+    // System.out.println("-".repeat(10));
+    // System.out.println(video1.toLink());
+    // System.out.println(video4.toLink());
+    // System.out.println("-".repeat(10));
+    // System.out.println("");
+
+    // // 5 testing toText method in User class
+    // System.out.println(yellow+"Testing toText method (User)"+exit);
+    // System.out.println("-".repeat(10));
+    // System.out.println(user1.toText());
+    // System.out.println(user2.toText());
+    // System.out.println(user3.toText());
+    // System.out.println("-".repeat(10));
+        
 }
